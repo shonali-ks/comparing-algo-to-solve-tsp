87833afff886b69177fa2ad2160163b38ab62f65
shonali-ks <50069308+shonali-ks@users.noreply.github.com>
2020-06-21 23:52:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,37 +9,37 @@ import time
 
 #naive algo
 print("\nNaive algorithm: \n\n")
-start = time.process_time()
+start1 = time.process_time()
 naive()
-end = time.process_time()
-print("\ntime taken for naive: ",end - start)
+end1 = time.process_time()
+print("\ntime taken for naive: ",end1 - start1)
 
 #dp_algo
 print("\nDynamic programming algorithm: \n\n")
-start = time.process_time()
+start2 = time.process_time()
 dp_algo() 
-end = time.process_time()
-print("time taken for Dynamic programming: ",end - start)
+end2 = time.process_time()
+print("time taken for Dynamic programming: ",end2 - start2)
 
 #bound and branch algo
-print("\nBound and branch algorithm: \n\n")
-start = time.process_time()
+print("\nBranch and bound algorithm: \n\n")
+start3 = time.process_time()
 bnb() 
-end = time.process_time()
-print("\ntime taken for bound and branch: ",end - start)
+end3 = time.process_time()
+print("\ntime taken for branch and bound : ",end3 - start3)
 
 
 #1.5 opt algorithm
 print("\n3/2 opt algorithm: \n\n")
-start = time.process_time()
+start4 = time.process_time()
 one_and_half_algo()
-end = time.process_time()
-print("time taken for 3/2 approximation algorithm: ",end - start)
+end4 = time.process_time()
+print("time taken for 3/2 approximation algorithm: ",end4 - start4)
 
 
 #genetic algorithm
 print("\nGenetic algorithm: \n\n")
-start = time.process_time()
+start5 = time.process_time()
 genetic()
-end = time.process_time()
-print("time taken for  Genetic algorithm for 50 iterations: ",end - start)
+end5 = time.process_time()
+print("time taken for  Genetic algorithm for 50 iterations: ",end5 - start5)
